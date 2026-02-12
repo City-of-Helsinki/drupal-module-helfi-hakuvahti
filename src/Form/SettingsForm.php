cfc6ea7dc76678ad82863ed982d1ef6ee6e24894
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-02-12 14:00:28 +0200

    UHF-12723: Add form for hakuvahti settings
    This was originally implemented in rekry, but I moved this to
    helfi_hakuvahti when we started to install hakuvahti to other instances.

---
+++ src/Form/SettingsForm.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_hakuvahti\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Hakuvahti settings form.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  use AutowireTrait;
+
+  public function __construct(
+    ConfigFactoryInterface $configFactory,
+    TypedConfigManagerInterface $typedConfigManager,
+  ) {
+    parent::__construct($configFactory, $typedConfigManager);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() : string {
+    return 'helfi_hakuvahti.settings_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() : array {
+    return ['helfi_hakuvahti.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['settings']['hakuvahti_tos_checkbox_label'] = [
+      '#type' => 'textfield',
+      '#config_target' => 'helfi_hakuvahti.settings:hakuvahti_tos_checkbox_label',
+      '#title' => $this->t('Hakuvahti terms of service checkbox label'),
+      '#description' => $this->t('Label for the terms of service checkbox.'),
+    ];
+
+    $form['settings']['hakuvahti_tos_link_text'] = [
+      '#type' => 'textfield',
+      '#config_target' => 'helfi_hakuvahti.settings:hakuvahti_tos_link_text',
+      '#title' => $this->t('Hakuvahti terms of service link text'),
+    ];
+
+    $form['settings']['hakuvahti_tos_link_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Hakuvahti terms of service URL'),
+      '#config_target' => 'helfi_hakuvahti.settings:hakuvahti_tos_link_url',
+      '#description' => $this->t('URL for the webpage or pdf to the Hakuvahti terms of service.'),
+    ];
+
+    $form['settings']['hakuvahti_instructions_link_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('More detailed instructions on how to use saved searches'),
+      '#config_target' => 'helfi_hakuvahti.settings:hakuvahti_instructions_link_url',
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+}
