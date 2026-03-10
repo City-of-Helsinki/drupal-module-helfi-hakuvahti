88ed6871850ed76f4c8cea1c3907c1fec13b0120
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-03-10 15:41:39 +0200

    UHF-12838: Fix variable name

--- src/HakuvahtiRequest.php
+++ src/HakuvahtiRequest.php
@@ -105,7 +105,7 @@ final readonly class HakuvahtiRequest {
       'site_id' => $this->siteId,
       'query' => $this->query,
       'elastic_query' => $this->elasticQuery,
-      'elastic_query_atv' => $this->elasticQueryAtv,
+      'user_data_in_atv' => $this->elasticQueryAtv,
       'search_description' => $this->searchDescription,
     ];
   }
